ac15d57747a5b37bf4e934e7a7e250bede5fbe20
Alex <alexandru.i.jurju@gmail.com>
2024-06-28 08:30:46 +0300

    Episode 8: Validation

--- BuberDiner/BuberDiner.Application/DependencyInjection.cs
+++ BuberDiner/BuberDiner.Application/DependencyInjection.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Reflection;
+using BuberDiner.Application.Authentication.Commands.Register;
+using BuberDiner.Application.Authentication.Common;
+using BuberDiner.Application.Common.Behaviours;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDiner.Application;
 
@@ -7,6 +14,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(x => x.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+
+
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
         return services;
     }
 }
